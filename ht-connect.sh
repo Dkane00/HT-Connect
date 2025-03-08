c3fa7be933ce9d013c49b240b6657814e31ad0e4
david <outlaw@kanenet.net>
2025-03-08 16:07:11 -0500

    created a ht-connect.sh script that should give everything a yad gui

---
+++ ht-connect.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Function to run a script with sudo password prompt
+run_script() {
+    local script=$1
+    local title=$2
+    yad --title="$title" --form --field="Enter sudo password:H" --button="OK" | \
+    awk -F'|' '{print $1}' | \
+    sudo -S bash "$script"
+}
+
+# Create the main GUI window
+yad --title="Script Launcher" --form \
+    --field="Connect Bluetooth:BTN" "bash -c 'run_script connect.sh \"Connect Bluetooth\"'" \
+    --field="Disconnect Bluetooth:BTN" "bash -c 'run_script disconnect.sh \"Disconnect Bluetooth\"'" \
+    --field="KISS Connect:BTN" "bash -c 'run_script kiss-connect.sh \"KISS Connect\"'" \
+    --field="Pairing:BTN" "bash -c 'run_script pairing.sh \"Pairing\"'" \
+    --field="TCP Connect:BTN" "bash -c 'run_script tcp-connect.sh \"TCP Connect\"'" \
+    --button="Exit" --width=400 --height=300
