84ae025af0626bd29b3929c1d165b4c4c3309993
david <outlaw@kanenet.net>
2025-03-09 10:37:09 -0400

    changed the connect item in the main gui from connect bluetooth to connect serial to make it more clear what it does

--- ht-connect.sh
+++ ht-connect.sh
@@ -3,7 +3,7 @@
 # Main GUI
 yad --title="HT Connect" --form \
     --field="Pairing:BTN" "bash -c './gui/htc-pair-gui.sh'" \
-    --field="Connect Bluetooth:BTN" "bash -c './gui/htc-connect-gui.sh'" \
+    --field="Connect Serial:BTN" "bash -c './gui/htc-connect-gui.sh'" \
     --field="KISS Connect:BTN" "bash -c './gui/htc-kiss-gui.sh'" \
     --field="TCP Connect:BTN" "bash -c './gui/htc-tcp-gui.sh'" \
     --field="Disconnect Bluetooth:BTN" "bash -c './gui/htc-disconnect-gui.sh'" \
